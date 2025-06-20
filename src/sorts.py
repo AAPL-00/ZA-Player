2c9667918742afb2e61573d510f9f7ee40bcb2a3
toni <aapl00@proton.me>
2025-06-20 10:12:14 -0400

    Add random_sort(repo: dict) to repo

--- src/sorts.py
+++ src/sorts.py
@@ -1,6 +1,6 @@
 import random
 
-def random_sort(repo: dict) -> list[str]:
-    paths = list(repo.keys())
-    random.shuffle(paths)
-    return paths
+def random_sort(repo: dict) -> list[tuple[str, str]]:
+    path_title_pairs = [(path, metadata[0]) for path, metadata in repo.items()]
+    random.shuffle(path_title_pairs)
+    return path_title_pairs
